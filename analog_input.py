52ba1ec3f66108699c6201fee609b68857ed496b
Kelly Cantrell <kellycantrelly@gmail.com>
2025-11-30 18:26:08 -0600

    Remove print statement

--- analog_input.py
+++ analog_input.py
@@ -5,11 +5,21 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 ADC0834.setup()
 
+# Setup GPIO Pin for LED
+outPin=20
+GPIO.setup(outPin,GPIO.OUT)
+myPWM=GPIO.PWM(outPin,100)
+
+# Starting Cycle - Set's default brightness and value to be modified
+brightness = 0
+myPWM.start(brightness)
+
 try:
     while True:
         analogVal=ADC0834.getResult(0)
-        print(analogVal)
+        #print(analogVal)
         sleep(.2)
+        myPWM.ChangeDutyCycle((analogVal * (100/255)))
 except KeyboardInterrupt:
     GPIO.cleanup()
     print("\nGPIO exited cleanly...")
