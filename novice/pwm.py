d5fdcb5ee0a3340f220dd52b71f04c4462cfebdd
Kelly Cantrell <kellycantrelly@gmail.com>
2026-09-07 13:35:52 -0500

    move stuff around, improve code

--- novice/pwm.py
+++ novice/pwm.py
@@ -1,46 +1,54 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python3
+"""
+Project that implements PWM to dim an LED
+with button switches to increase / decrease brightness
+"""
+import RPi.GPIO as gp
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
+gp.setmode(gp.BCM)
 
-# Setup GPIO Pin for LED
-outPin=38
-GPIO.setup(outPin,GPIO.OUT)
-myPWM=GPIO.PWM(outPin,100)
+# Setup gp Pin for LED
+out_pin=16
+gp.setup(out_pin,gp.OUT)
+pwm_pin=gp.PWM(out_pin,100)
 
 # Setup pins for brightness
-dimmer=11
-GPIO.setup(dimmer,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-upper=16
-GPIO.setup(upper,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+dim_pin=26
+gp.setup(dim_pin,gp.IN,pull_up_down=gp.PUD_UP)
+bright_pin=13
+gp.setup(bright_pin,gp.IN,pull_up_down=gp.PUD_UP)
 
 # Starting Cycle - Set's default brightness and value to be modified
-brightness = 0
-myPWM.start(brightness)
+duty_cycle = 0
+pwm_pin.start(duty_cycle)
 
 try:
     while True:
-        dimVal = GPIO.input(dimmer)
-        upVal = GPIO.input(upper)
-        if dimVal == 0 and upVal == 0:
+        # read button presses - 0 = press
+        dim_val = gp.input(dim_pin)
+        bright_val = gp.input(bright_pin)
+
+        if dim_val == 0 and bright_val == 0:
             sleep(0.2)
             print("BOTH BUTTONS PRESSED --- NO CHANGE")
-        if dimVal == 0:
+        elif dim_val == 0:
             sleep(0.2)
-            if brightness == 0:
+            if duty_cycle == 0:
                 pass
             else:
-                brightness -= 20
-            print("DOWN")
-        if upVal == 0:
+                duty_cycle -= 20
+            print(duty_cycle)
+        elif bright_val == 0:
             sleep(0.2)
-            if brightness == 100:
+            if duty_cycle == 100:
                 pass
             else:
-                brightness += 20
-            print("UP")
+                duty_cycle += 20
+            print(duty_cycle)
 
-        myPWM.ChangeDutyCycle(brightness)
-
+        pwm_pin.ChangeDutyCycle(duty_cycle)
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    print("\n")
+finally:
+    gp.cleanup()
